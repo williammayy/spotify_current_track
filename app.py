b2d3c416c09807054fef4d9812c4d7ec6a0362fc
William May <williammayy@gmail.com>
2023-07-23 22:11:01 -0300

    only show track if it has changed

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 import spotipy
+import time
 from spotipy.oauth2 import SpotifyOAuth
 
 scope = "user-read-playback-state"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-results = sp.current_playback()
+previous_song = ""
+current_song = ""
 
-if results:
-    track = results['item']['name']
-    album = results['item']['album']['name']
-    artists = results['item']['artists'][0]['name']
-    album_cover = results['item']['album']['images'][0]['url']
-    print(f"{track}, {artists} - {album} \n{album_cover}")
-else:
-    print("Nothing is playing...")
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+while True:
+    results = sp.current_playback()
+    if results is None:
+        current_song = 'none'
+        if previous_song != current_song:
+            previous_song = 'none'
+            print("There is no song playing...")
+    else:
+        current_song = results['item']['name']
+        if previous_song != current_song:
+            track = results['item']['name']
+            album = results['item']['album']['name']
+            artists = results['item']['artists'][0]['name']
+            album_cover = results['item']['album']['images'][0]['url']
+            previous_song = results['item']['name']
+
+            print(f"{track}, {artists} - {album} \n{album_cover}")
+
+    time.sleep(5)
